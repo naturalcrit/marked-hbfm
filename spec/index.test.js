f8667fedad5950f456cd9b649ab76adc2af7abf4
David Bolack <dbolack@gmail.com>
2026-08-25 20:17:49 -0500

    Add the tests

---
+++ spec/index.test.js
@@ -0,0 +1,26 @@
+import { hbfm } from  '../src/index.js';
+
+function trimLines(s) {
+	return s.split('\n').map((l)=>l.trim()).join('\n');
+}
+
+
+describe('Replacement Underline Interactions', ()=>{
+	test('Underlined Text', function() {
+		const source = 'The next word should be _underlined._\n';
+		const rendered = Markdown.render(source).trim();
+		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>The next word should be <u>underlined.</u></p>`);
+	});
+
+	test('Previously Bolded Underscored Text', function() {
+		const source = 'The next word should be __normal.__\n';
+		const rendered = Markdown.render(source).trim();
+		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>The next word should be normal.</p>`);
+	});
+
+	test('Previously Bolded and Emphasized Underscored Text', function() {
+		const source = 'The next word should be ___normal.___\n';
+		const rendered = Markdown.render(source).trim();
+		expect(rendered, `Input:\n${source}`, { showPrefix: false }).toBe(`<p>The next word should be normal.</p>`);
+	});
+});
