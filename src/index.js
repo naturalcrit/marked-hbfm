849bc4fd8da25249ed479f1584ec6cea5ae8ce79
David Bolack <dbolack@gmail.com>
2026-08-25 19:54:49 -0500

    Emulate  https://github.com/naturalcrit/homebrewery/pull/4721
    Standing issue with Jest

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { markedSmartypantsLite as MarkedSmartypantsLite }                       
 import { gfmHeadingId as MarkedGFMHeadingId, resetHeadings as MarkedGFMResetHeadingIDs } from 'marked-gfm-heading-id';
 import { markedEmoji as MarkedEmojis }                                                   from 'marked-emoji';
 import MarkedDiagramsMarkdeep from 'marked-diagrams-markdeep';
-
+import MarkedUnderline from 'marked-underline';
 
 //Icon fonts included so they can appear in emoji autosuggest dropdown
 import diceFont      from './diceFont.js';
@@ -358,6 +358,7 @@ const markdeepOptions = { langs: ['asciiArt'] };
 Marked.use(markedVariables());
 Marked.use(MarkedDiagramsMarkdeep(markdeepOptions));
 Marked.use(MarkedDefinitionLists());
+Marked.use(MarkedUnderline({ justUnderline: true }));
 Marked.use({ extensions: [forcedParagraphBreaks, mustacheSpans, mustacheDivs, mustacheInjectInline] });
 Marked.use(mustacheInjectBlock);
 Marked.use(MarkedAlignedParagraphs());
@@ -366,6 +367,7 @@ Marked.use(MarkedNonbreakingSpaces());
 Marked.use({ renderer: renderer, tokenizer: tokenizer, mangle: false });
 Marked.use(MarkedExtendedTables({ interruptPatterns: tableTerminators }), MarkedGFMHeadingId({ globalSlugs: true }),
 	MarkedSmartypantsLite(), MarkedEmojis(MarkedEmojiOptions));
+  
 
 function cleanUrl(href) {
 	try {
